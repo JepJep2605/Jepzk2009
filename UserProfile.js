a72f945ab054aaa15bbd90aa0dbd5be0a50ea87e
JEFFREY R. RAGIL <jeffreyragil6@gmail.com>
2024-11-04 11:18:30 +0000

    initial commit

---
+++ UserProfile.js
@@ -0,0 +1,8 @@
+const userProfile = {
+      fullName: "John Doe",
+        age: 25,
+          favoriteNumber: 7,
+            favoriteColors: ["blue", "green", "red"]
+            };
+
+            console.log(userProfile);
